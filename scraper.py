4b7e61317939c764583849ca7bd2f3f4dc4c88df
RWilletts92 <43743626+RWilletts92@users.noreply.github.com>
2019-02-19 12:34:22 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,9 +18,13 @@ root = lxml.html.fromstring(html)
 print(root.cssselect("div#footer"))
 print(root)
 listofmatches=root.cssselect('a')
+record=()
 for match in listofmatches:
   print(match)
   print(lxml.html.tostring(match))
+  record['link']=lxml.html.tostring(match)
+  print(record)
+  scraperwiki.sqlite.save(unique_keys=['link'], data=record)
 # # div is a HTML tag, a is the tag a for anchor, href is an attribute they have values, align is attribute
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
